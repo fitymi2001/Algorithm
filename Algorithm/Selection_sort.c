dc861d17450d4ee3de79b9796ae0eed64c385910
fitymi2001 <fitymi2001@gmail.com>
2023-10-19 16:37:12 +0900

    선택 정렬 완성입니다.

--- Algorithm/Selection_sort.c
+++ Algorithm/Selection_sort.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World!");
-	return 0;
+	int arr[5] = { 5, 4, 3, 2, 1 }, tmp;
+
+	// print arr
+
+	printf("arr = ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d", arr[i]);
+	}
+
+	// start sort
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = i + 1; j < 5; j++) {
+			if (arr[i] > arr[j]) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+
+	// print arr
+
+	printf("\narr = ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d", arr[i]);
+	}
 }
